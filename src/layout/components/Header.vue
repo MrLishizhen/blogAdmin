45b83fc57dbb8f8feb50c71447e60beaaf64ad70
秋雨不良人 <718647063@qq.com>
2020-12-27 23:21:15 +0800

    首页logo

--- src/layout/components/Header.vue
+++ src/layout/components/Header.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>你好</div>
+    <div class="header">
+        <div class="header-logo">
+
+        </div>
+        <div class="header-list"></div>
+        <div class="header-user"></div>
+    </div>
 </template>
 
 <script>
@@ -8,6 +14,16 @@
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+    .header{
+        height:100%;
+        display:flex;
+        align-items: center;
+        .header-logo{
+            width:60px;
+            height:30px;
+            border-radius: 3px;
+            background:url("../../assets/home/logo.png") no-repeat center/100%;
+        }
+    }
 </style>
